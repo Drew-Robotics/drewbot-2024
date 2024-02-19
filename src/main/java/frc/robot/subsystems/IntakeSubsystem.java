2ac68bd2070fea989415d2e37c9ba85dd6d5d8a8
nmurphy <nmurphy.mine@gmail.com>
2024-02-19 11:49:38 -0800

    intake fixes

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -2,18 +2,19 @@ package frc.robot.subsystems;
 
 import frc.robot.Constants.IntakeConstants;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
-public class Intake extends SubsystemBase {
+public class IntakeSubsystem extends SubsystemBase{
   private final PIDController m_pivotPID = new PIDController(IntakeConstants.kPivotP, IntakeConstants.kPivotI, IntakeConstants.kPivotD);
   
-  private static Intake m_instance;
+  private static IntakeSubsystem m_instance;
   
-  public static Intake getInstance() {
-    if (m_instance = null) {
-      m_instance = new Intake();
+  public static IntakeSubsystem getInstance() {
+    if (m_instance == null) {
+      m_instance = new IntakeSubsystem();
     }
     return m_instance;
   }
@@ -21,13 +22,13 @@ public class Intake extends SubsystemBase {
   private CANSparkMax m_intakeMotor;
   private CANSparkMax m_pivotMotor;
 
-  private Intake() {
+  private IntakeSubsystem() {
     // Intake Motor
     m_intakeMotor = new CANSparkMax(IntakeConstants.kIntakeMotorID, MotorType.kBrushless);
     m_intakeMotor.restoreFactoryDefaults();
     m_intakeMotor.setIdleMode(CANSparkMax.IdleMode.kCoast); // Maybe have to change this later?
     // Pivot Motor
-    m_pivotMotor = new CANSparkMax(IntakeConstants.kPivotMotorID)
+    m_pivotMotor = new CANSparkMax(IntakeConstants.kPivotMotorID, MotorType.kBrushless);
     
   }
 }
